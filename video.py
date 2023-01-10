1723ffc954e342702adecdd2b6a611577abc1eb2
Aron <aron17marton@gmail.com>
2023-01-10 21:42:27 +0100

    Initial commit

--- video.py
+++ video.py
@@ -6,8 +6,8 @@ import cv2
 import numpy as np
 from moviepy.editor import *
 
-clip = VideoFileClip('Chill Minecraft Hypixel parkour gameplay for commentary! (free to use).mp4')
+clip = VideoFileClip('clips/Chill Minecraft Hypixel parkour gameplay for commentary! (free to use).mp4')
 
-clip = clip.subclip(0, 10)
+clip = clip.subclip(0, 12)
 
-clip.write_videofile("clip.mp4")
+clip.write_videofile("clips/clip.mp4")
